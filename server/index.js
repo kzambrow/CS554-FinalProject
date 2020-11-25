b1485e3443d9689f4cf18a13fef74fb2d77f8ea2
Yunxiang Fan <yunxiangfan@YunxiangdeMacBook-Pro.local>
2020-11-25 04:53:14 -0600

    Getting start

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const configRoutes = require('./routes');
+
+const app = express();
+
+app.use();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.engine('handlebars', handlebarsInstance.engine);
+app.set('view engine', 'handlebars');
+
+configRoutes(app);
+
+app.listen(3000, () => {
+  console.log("We've now got a server!");
+  console.log('Your routes will be running on http://localhost:3000');
+});
