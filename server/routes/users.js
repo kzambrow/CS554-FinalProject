26cac6aa3a483ca27ab70bce264d5d02a44e0164
kzambrow <kzambrow@stevens.edu>
2020-12-14 22:34:41 -0500

    trying to get image upload to work

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const multer = require('multer');
 const router = express.Router();
 const User = require('../models/user-model');
+
 
 router.post("/addUser", async (req, res) => {
     const body = req.body;
