83f2a1dd2afeab56a85a5a62332be894e89768c9
Yunxiang Fan <yunxiangfan@YunxiangdeMacBook-Pro.local>
2020-12-17 11:42:00 -0600

    queue implemented

--- client/src/components/PostPage.js
+++ client/src/components/PostPage.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState, useContext } from 'react';
 import '../App.css';
-import noImage from '../img/no-image.png';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { Card, CardActionArea, CardContent, CardMedia, Grid, Typography, makeStyles, Button } from '@material-ui/core';
 import { AuthContext } from '../firebase/Auth';
+import GetWaitingInfo from './GetWaitingInfo';
+import { Beforeunload } from 'react-beforeunload';
 
 const axios = require('axios');
+
 
 const useStyles = makeStyles({
     card: {
@@ -42,46 +44,66 @@ const useStyles = makeStyles({
 //display different information for authenticated vs non authenticated
 //ask about updating user schema to include island code, nintendo ID
 
+
 function PostPage(props) {
     const postId = props.match.params.id;
-
-    const GetPost = () => {
-        const classes = useStyles();
-        const [loading, setLoading] = useState(true);
-        const [postData, setPostData] = useState(undefined);
-        const { currentUser } = useContext(AuthContext);
-
-
-        useEffect(() => {
-            async function fetchData() {
-                try {
-                    //getting data for user profile
-                    const post = await axios.get("http://localhost:5000/post/" + postId);
-                    setPostData(post.data.data);
-                    const queue = await axios.post("http://localhost:5000/queue/join", {
-                        userId: currentUser.id,
-                        postId: postId
-                    })
-                    setLoading(false);
-                } catch (e) {
-                    console.log(e);
-                }
-            }
-            fetchData();
-        }, []);
+    const classes = useStyles();
+    const [loading, setLoading] = useState(true);
+    const [postData, setPostData] = useState(undefined);
+    const [queueId, setQueueId] = useState(undefined);
+    const { currentUser } = useContext(AuthContext);
 
 
-        if (loading) {
-            return (
-                <div>
-                    <h2>Loading....</h2>
-                </div>
-            );
+    useEffect(() => {
+
+        const cleanup = () => {
+            console.log('removing from queue!!!')
+            axios.post('http://localhost:5000/queue/leave', { queueId: queueId })
+
         }
-        else {
-            return (
-                //This only appears on Page 1 
-                <div>
+        window.addEventListener('beforeunload', cleanup);
+
+        async function fetchData() {
+            try {
+                //getting data for user profile
+                const post = await axios.get("http://localhost:5000/post/" + postId);
+                setPostData(post.data.data);
+                const queue = await axios.post("http://localhost:5000/queue/join", {
+                    userId: currentUser.id,
+                    postId: postId,
+                    inGameName: currentUser.inGameName
+                })
+                setQueueId(queue.data.data._id);
+                setLoading(false);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+        fetchData();
+        return () => {
+            window.removeEventListener('beforeunload', cleanup);
+        }
+    }, []);
+
+
+
+    if (loading) {
+        return (
+            <div>
+                <h2>Loading....</h2>
+            </div>
+        );
+    }
+    else {
+        // function handleModalLeave(){
+        //     axios.post('http://localhost:5000/queue/',{queueId: queueId});
+        // }
+        return (
+            <div>
+                <Beforeunload onBeforeunload={() => {
+                    axios.post('http://localhost:5000/queue/leave', { queueId: queueId })
+
+                }}>
                     <br />
                     <br />
                     {/* <Button> <Link to={"/sell"}> Selling </Link> </Button> */}
@@ -89,21 +111,19 @@ function PostPage(props) {
 
                     <Card className={classes.card} variant='outlined'>
                         <CardActionArea>
-
-                            {/* <CardMedia
-                                className={classes.media}
-                                component='img'
-                                image={show.image && show.image.original ? show.image.original : noImage}
-                                title='show image'
-                            /> */}
-
                             <CardContent>
 
                                 <Typography variant='body2' color='textSecondary' component='p'>
                                     <br></br>
                                         Description: {postData.description}
                                     <br></br>
-                                        Ticket Price: {postData.ticketPrice}
+                                        Ticket Price: {postData.ticketPrice}<br />
+                                    <br />
+                                    <Button onClick={async() => {
+                                        await axios.post('http://localhost:5000/queue/leave', { queueId: queueId });
+                                        
+                                    }}><Link to= {"/"}>Leave Island</Link></Button>
+
                                     {/* <br></br>
                                         Rating: {show.rating}
                                     <br></br>
@@ -115,80 +135,15 @@ function PostPage(props) {
 
                         </CardActionArea>
                     </Card>
-
-                </div>
-            );
-        }
-    };
-    function GetCode() {
-
-        const { currentUser } = useContext(AuthContext);
-        const [islandCode, setIslandCode] = useState(undefined);
-        const classes = useStyles();
-        useEffect(() => {
-            const getCode = async () => {
-                // const userInfo = await axios.get(`http://localhost:5000/user/email/${currentUser.email}`);
-                try {
-                    const queue = await axios.post('http://localhost:5000/queue/find', {
-                        userId: currentUser.id,
-                        postId: postId
-                    })
-                    const code = await axios.post('http://localhost:5000/queue/getCode', {
-                        postId: postId,
-                        queueId: queue.data.data._id
-                    })
-                    setIslandCode(code.data.data);
-                } catch (e) {
-                    console.log(e.response)
-                }
-            }
-            getCode();
-
-        }, []);
-        let card = null;
-
-        islandCode && (card =
-            <Card className={classes.card} variant='outlined'>
-                <CardActionArea>
-                    {/* <CardMedia
-                        className={classes.media}
-                        component='img'
-                        image={noImage}
-                        title='show image'
-                    /> */}
-                    <CardContent>
-                        <Typography variant='body2' color='textSecondary' component='p'>
-                            <br></br>
-                            Code: {islandCode}
-                            <br></br>
-                            Island Code: ABCD
-                            <br></br>
-                            Ingame Name: GAMER1
-                            <br></br>
-                            Nintendo ID: 1234567
-
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-            </Card>
-        );
-
-        return (
-            <Grid container className={classes.grid} spacing={5}>
-                {card}
-            </Grid>
+                    <br />
+                    <GetWaitingInfo queueId={queueId} postId={postId} />
+                    <Link className='showlink' to='/'>Homepage</Link>
+                </Beforeunload >
+            </div>
 
         );
-    };
-    return (
-        <div>
-            <GetPost />
-            <GetCode />
-            <br />
-            <Link className='showlink' to='/'>Homepage</Link>
+    }
 
-        </div>
-    );
 }
 
 export default PostPage
